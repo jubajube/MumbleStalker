2807b34329b4ea081b4fcbe513f0b8b20a58feb0
Richard Walters <jubajube@gmail.com>
2015-11-09 00:03:24 -0800

    Fix application icon and fill in assembly attributes

--- MumbleStalkerWin/Properties/AssemblyInfo.cs
+++ MumbleStalkerWin/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MumbleStalkerWin")]
+[assembly: AssemblyTitle("Mumble Stalker for Windows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MumbleStalkerWin")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Mumble Stalker")]
+[assembly: AssemblyCopyright("Copyright © 2015 Richard Walters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
